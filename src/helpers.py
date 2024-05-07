9c0a1735aea49222bf0edc5d7a3ef35ef41062b3
Kucharssim <kucharssim@gmail.com>
2024-05-07 08:46:38 +0200

    rerun examples

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def classification_entropy(p):
     log_p = np.log(p)
@@ -17,3 +18,14 @@ def accuracy(p, latents):
     cls = tf.one_hot(np.argmax(p, axis=-1), p.shape[-1])
     acc = np.mean(latents == cls, axis=1)
     return acc
+
+
+def plot_bands(x, samples, probs=[0.5, 0.9], color=None, alpha=None, axis=0):
+    for p in probs:
+        plt.fill_between(
+            x,
+            np.quantile(samples, q=1-p/2.0, axis=axis),
+            np.quantile(samples, q=p/2.0, axis=axis),
+            alpha=alpha,
+            color=color
+        )
