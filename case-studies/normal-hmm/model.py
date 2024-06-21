6c36f2e241af0874529fd896cafe2097fa2f377d
Kucharssim <kucharssim@gmail.com>
2024-06-21 16:01:50 +0200

    rerun stan calibration

--- case-studies/normal-hmm/model.py
+++ case-studies/normal-hmm/model.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.abspath(os.path.join("../..")))
 from src.models.NormalHmm import NormalHmm
 
 
-model = NormalHmm(n_cls = 2, n_obs=[100, 100], separation=3.0, alpha=0.5)
+model = NormalHmm(n_cls = 2, n_obs=[100, 100], separation=3.0, alpha=2)
 model.prior_means = [0.0, 0.0, -1.5, 1.00]
-model.prior_sds = [3.12,  3.12, 1.0, 0.65]
+model.prior_sds = [1.12,  1.12, 1.0, 0.65]
