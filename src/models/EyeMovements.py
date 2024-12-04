1958ab69e70d94d1ee787baa17f54f2677a6ad1a
Kucharssim <kucharssim@gmail.com>
2024-12-04 16:25:56 +0100

    basic generative model

--- src/models/EyeMovements.py
+++ src/models/EyeMovements.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
+from bayesflow.simulation import Prior, Simulator, GenerativeModel
+
+
 class Updateable():
     def __init__(self, position: np.ndarray, dt=1.0):
         self.dt = dt
@@ -22,8 +25,7 @@ class Updateable():
 
 class Fixation(Updateable):
     def __init__(self, position: np.ndarray, target: np.ndarray, eye_drift: float, eye_tremor: float, dur_threshold: float, dur_drift: float, dt=1.0):
-        super().__init__(dt = dt)
-        self.position = position
+        super().__init__(position=position, dt = dt)
         self.target = target
 
         self.eye_drift = eye_drift
@@ -33,8 +35,8 @@ class Fixation(Updateable):
 
     def __call__(self):
         accumulator = 0.0
-        while accumulator < self.threshold:
-            accumulator = accumulator + np.random.normal(loc=self.drift_rate) * self.dt
+        while accumulator < self.dur_threshold:
+            accumulator = accumulator + np.random.normal(loc=self.dur_drift) * self.dt
             self.update()
             self.snapshot("fixation")
 
@@ -44,6 +46,7 @@ class Fixation(Updateable):
 
         self.velocity = (drift + tremor) * self.dt
         self.position = (self.position) + self.velocity
+        self.time = self.time + self.dt
 
 class Saccade(Updateable):
     def __init__(
@@ -98,12 +101,12 @@ class Saccade(Updateable):
 
         burst = self.burst(t, a)
         drag = self.beta * self.velocity
+        #drag = np.zeros(2)
 
         cn = np.random.normal(size=2, scale=self.cn)
         sdn_a = np.random.normal(size=2, scale=(self.sdn * burst))
         sdn_b = np.random.normal(size=2, scale=(self.sdn * b))
-
-
+            
         acc = (burst - b - drag + cn + sdn_a + sdn_b) * self.dt
 
         self.velocity = self.velocity + acc * self.direction
@@ -191,22 +194,103 @@ class Saccade(Updateable):
     
 
 class Eye(Updateable):
-    def __init__(self, position: np.ndarray, fixation_params: dict, saccade_params: dict, dt = 1):
-        super().__init__(dt = dt)
-        self.position = position
+    def __init__(self, fixation_params: dict, saccade_params: dict, dt = 1):
 
         self.display_min = np.zeros(2)
         self.display_max = np.array([1024,  768])
 
+        self.position = np.random.uniform(low = self.display_min, high = self.display_max)
+        self.history = {"position": []}
         self.fixation_params = fixation_params
         self.saccade_params = saccade_params
+        self.dt = dt
+        self.time = 0.0
+
+    def __call__(self, max_time=10_000):
+        target = self.position
+        while self.time <= max_time:
+            print("new round, time", self.time)
+            fixation = Fixation(position=self.position, target=target, dt=self.dt, **self.fixation_params)
+            fixation()
+            self.position = fixation.position
+            print("fixation done")
+            target = np.random.uniform(low = self.display_min, high = self.display_max)
+            saccade = Saccade(position=self.position, target=target, dt=self.dt, **self.saccade_params)
+            saccade()
+            self.position = saccade.position
+            print("saccade done")
+            self.time = self.time + fixation.time + saccade.time
+            self.snapshot(fixation, saccade)
+
+
+    def snapshot(self, fixation: Fixation, saccade: Saccade):
+        fp = fixation.history["position"]
+        self.history['position'].append(np.array(fp))
+        sp = saccade.history["position"]
+        self.history['position'].append(np.array(sp))
+
+
 
 
 if __name__ == "__main__":
-    saccade = Saccade(position=np.zeros(2), target=np.array([150, 70]), alpha=0.1, beta=0.05, sdn=0.5, cn=0.5)
-    t, a, b = saccade()
+    fixation_params = {"eye_drift": 0.1, "eye_tremor": 25.0, "dur_threshold": 1000, "dur_drift": 3}
+    saccade_params = {"alpha": 0.2, "beta": 0.01, "cn": 0.1, "sdn": 0.2}
 
-    position = np.array(saccade.history['position'])
-    time = np.array(saccade.history['time'])
-    plt.plot(time, position)
+
+    # saccade = Saccade(position=np.array([348.0685267, 729.03394094]), target=np.array([727.58391991, 470.95124244]), **saccade_params)
+    # t, a, b = saccade()
+
+    # position = np.array(saccade.history['position'])
+    # time = np.array(saccade.history['time'])
+    # plt.plot(time, position)
+    # plt.show()
+
+
+    eye = Eye(fixation_params=fixation_params, saccade_params=saccade_params)
+    eye(max_time=5_000)
+    position = np.vstack(eye.history['position'])
+    print(position.shape)
+
+    plt.plot(position)
     plt.show()
+
+
+
+def prior_fun():
+    drift = np.random.lognormal(mean=0.05, sigma=1)
+    tremor = np.random.lognormal(mean=5, sigma=1)
+    threshold = np.random.lognormal(mean=100, sigma=5)
+    drift_rate = np.random.lognormal(mean=2, sigma=0.5)
+
+    alpha = np.random.gamma(shape=5, scale=0.1)
+    beta  = np.random.gamma(shape=3, scale=0.003)
+    cn    = np.random.gamma(shape=2, scale=0.2)
+    sdn   = np.random.gamma(shape=2, scale=0.2)
+
+
+    pars = np.r_[drift, tremor, threshold, drift_rate, alpha, beta, cn, sdn]
+
+    return pars
+
+
+prior = Prior(prior_fun=prior_fun)
+
+def simulator_fun(theta):
+    drift, tremor, threshold, drift_rate, alpha, beta, cn, sdn = theta
+
+    eye = Eye(
+        fixation_params = {"eye_drift": drift, "eye_tremor": tremor, "dur_threshold": threshold, "dur_drift": drift_rate},
+        saccade_params  = {"alpha": alpha, "beta": beta, "cn": cn, "sdn": sdn}
+    )
+
+    eye(max_time=10_000)
+
+    position = np.vstack(eye.history['position'])
+
+    return position
+
+
+simulator = Simulator(simulator_fun=simulator_fun)
+
+
+model = GenerativeModel(prior=prior, simulator=simulator, skip_test=True)
